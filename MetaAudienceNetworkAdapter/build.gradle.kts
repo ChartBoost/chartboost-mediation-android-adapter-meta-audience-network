69742ad670004852d3e436728489720aed298519
Jose Padilla <jose.padilla@chartboost.com>
2023-09-15 10:47:20 -0700

    [HB-6443] Meta Audience Network 6.16.0 (#40)
    - Updated Meta Audience Network SDK version to 6.16.0.
    - Updated Changelog, README, build script to 4.6.16.0.0.

--- MetaAudienceNetworkAdapter/build.gradle.kts
+++ MetaAudienceNetworkAdapter/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 21
         targetSdk = 33
         // If you touch the following line, don't forget to update scripts/get_rc_version.zsh
-        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "4.6.15.0.1"
+        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "4.6.16.0.0"
         buildConfigField("String", "CHARTBOOST_MEDIATION_META_AUDIENCE_NETWORK_ADAPTER_VERSION", "\"${android.defaultConfig.versionName}\"")
 
         consumerProguardFiles("proguard-rules.pro")
@@ -71,7 +71,7 @@ dependencies {
     "remoteImplementation"("com.chartboost:chartboost-mediation-sdk:4.0.0")
 
     // Partner SDK
-    implementation("com.facebook.android:audience-network-sdk:6.15.0")
+    implementation("com.facebook.android:audience-network-sdk:6.16.0")
 
     // Adapter Dependencies
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
