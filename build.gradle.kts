63aa8936fc879289101e8217216576802e98c410
Bichen Wang <bichenwang@gmail.com>
2024-12-13 03:43:07 -0800

    kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ buildscript {
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("com.android.library") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
 
 task<Delete>("clean") {
